be1c8e65f30ccc149571f43c48095577b748eef4
Keith <Keith@PLATO>
2017-02-10 18:08:29 -0700

    Add project files.

---
+++ SimpleClasses/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myNewCar = new Car();
+
+            myNewCar.Color = "Blue";
+            myNewCar.Make = "Audi";
+            myNewCar.Year = 2016;
+            myNewCar.Model = "A4";
+
+
+            Console.WriteLine("{0} - {1} - {2}", 
+                myNewCar.Make, 
+                myNewCar.Model, 
+                myNewCar.Year );
+
+            double value = myNewCar.DetermineMarketValue();
+
+            Console.WriteLine(value);
+                Console.ReadLine();
+
+
+
+        }
+
+        private static double determineValue (Car car)
+        {
+            double carValue = 100;
+            return carValue;
+
+        }
+    }
+
+    class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        
+
+        public double DetermineMarketValue()
+
+        { 
+             double carValue = 100;
+
+            if (this.Year > 2000)
+                carValue = 20000;
+            else
+                carValue = 2000;
+
+
+            return carValue;
+        }
+    }
+
+
+
+}
